eadfb09aa6d3d1670c6e66313ae618290a3e8d20
Benjamin S. Baumer <ben.baumer@gmail.com>
2020-11-10 20:20:02 +0000

    version 0.2.3

--- R/macros.R
+++ R/macros.R
@@ -2,10 +2,37 @@
 #' @name macros
 #' @export
 #' @param x text to wrap in macro
+#' @param index add LaTeX indexing?
 #'
-func <- function(x) {
-  index_entry('R functions', x)
-  paste0("`", x, "()`")
+func <- function(x, index = TRUE) {
+  word <- paste0(x, "()")
+  md <- paste0("`", word, "`")
+  if (index) {
+    return(paste0(index_entry('R', word), md))
+  } else {
+    return(md)
+  }
+}
+
+
+#' @rdname macros
+#' @export
+sql_func <- function(x) {
+  word <- paste0(toupper(x), "()")
+  paste0(
+    index_entry('subject', paste0('SQL!', word)),
+    paste0("`", word, "`")
+  )
+}
+
+#' @rdname macros
+#' @export
+sql_word <- function(x) {
+  word <- toupper(x)
+  paste0(
+    index_entry('subject', paste0('SQL!', word)),
+    paste0("`", word, "`")
+  )
 }
 
 #' @rdname macros
@@ -24,10 +51,14 @@ variable <- function(x) {
 
 #' @rdname macros
 #' @export
-pkg <- function(x) {
-  index_entry('R packages', x)
-  paste0("**", x, "**")
-  # add citation?
+pkg <- function(x, index = TRUE) {
+  word <- paste0("library(", x, ")")
+  md <- paste0("**", x, "**")
+  if (index) {
+    return(paste0(index_entry('R', word), md))
+  } else {
+    return(md)
+  }
 }
 
 #' @rdname macros
@@ -39,16 +70,27 @@ mdsr_data <- function(x) {
 
 #' @rdname macros
 #' @export
-person <- function(x) {
-  index_entry('subject', x)
-  paste0("[", x, "](https://en.wikipedia.org/w/index.php?search=", x, ")")
+mdsr_person <- function(x) {
+  # people need to be manually indexed, or function written to turn Ben Baumer into Baumer, Ben
+  y <- stringr::str_split(x, " ")[[1]]
+  if (length(y) == 2) {
+    x2 <- paste0(rev(y), collapse = ", ")
+    index <- index_entry(index_label = 'subject', x2)
+  } else {
+    index <- paste0(
+      index_entry(index_label = 'subject', x)
+    )
+  }
+  part3 <- paste0("[", x, "](https://en.wikipedia.org/w/index.php?search=", x, ")")
+  return(paste0(index, part3))
 }
 
 #' @rdname macros
 #' @export
 vocab <- function(x) {
-  index_entry('subject', x)
-  paste0("[*", x, "*](https://en.wikipedia.org/w/index.php?search=", x, ")")
+  part1 <- index_entry(index_label = 'subject', x)
+  part2 <- paste0("[*", x, "*](https://en.wikipedia.org/w/index.php?search=", x, ")")
+  return(paste0(part1, part2))
 }
 
 #' @rdname macros
